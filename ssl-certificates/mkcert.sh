42a9442589afef541af04d9628e4a36c373e46c4
Brian Sidebotham <brian.sidebotham@gmail.com>
2020-09-15 12:56:25 +0100

    Update ssl certificate generation script for multiple SAN entries

--- ssl-certificates/mkcert.sh
+++ ssl-certificates/mkcert.sh
@@ -2,38 +2,139 @@
 
 # (c)2019 Brian Sidebotham <brian.sidebotham@gmail.com>
 
+logv() {
+  if ${verbose}; then
+    echo "VERBOSE: ${@}"
+  fi
+}
+
 if [ $# -lt 1 ]; then
-    echo "usage: $0 server.domain.name" >&2
+    echo "usage: $0 --domain server.domain.name [--alt dns names] [--ou Organisational Unit]" >&2
     exit 1
 fi
 
-domain=${1}
-
+domain=
+altnames=
+verbose=false
 C=GB
 ST=Bracknell
 O=BJS
+OU=
 CN=${domain}
 
+while [ $# -gt 0 ]; do
+    case "${1}" in
+        --domain)
+            shift
+            domain="${1}"
+            ;;
+
+        --ou)
+            shift
+            OU="${1}"
+            ;;
+
+        --o)
+            shift
+            O="${1}"
+            ;;
+
+        --st)
+            shift
+            ST="${1}"
+            ;;
+
+        --cn)
+            shift
+            CN="${1}"
+            ;;
+
+        --c)
+            shift
+            C="${1}"
+            ;;
+
+        --alt)
+            shift
+            altnames="${1}"
+            ;;
+
+        --verbose|-v)
+            verbose=true
+            ;;
+    esac
+
+    shift
+done
+
+if [ "${domain}X" = "X" ]; then
+    echo "domain must be set!" >&2
+    exit 1
+fi
+
+CN=${domain}
+
+subject=""
+
+if [ "${C}X" != "X" ]; then
+    subject="${subject}/C=${C}"
+fi
+
+if [ "${ST}X" != "X" ]; then
+    subject="${subject}/ST=${ST}"
+fi
+
+if [ "${O}X" != "X" ]; then
+    subject="${subject}/O=${C}"
+fi
+
+if [ "${OU}X" != "X" ]; then
+    subject="${subject}/OU=${OU}"
+fi
+
+if [ "${CN}X" != "X" ]; then
+    # For the root certificate, allow it to wildcard cover the domain
+    rootsubject="${subject}/CN=*.$(echo ${CN} | cut -d. -f2-)"
+    subject="${subject}/CN=${CN}"
+fi
+
 if [ ! -f rootca.key ]; then
+    logv "Generating rootca.key"
     openssl genrsa -des3 -out rootca.key 4096
 fi
 
 if [ ! -f rootca.crt ]; then
+    logv "Generating rootca.crt: ${rootsubject}"
+
     # Make the root CA a wildcard for a single level subdomain
     # Haven't bothered with adding in the apex too
     openssl req -x509 -new -nodes \
         -key rootca.key \
-        -subj "/C=${C}/ST=${ST}/O=${O}/CN=*.$(echo ${CN} | cut -d. -f2-)" \
+        -subj "${rootsubject}" \
         -sha256 \
         -days 1024 \
         -out rootca.crt
 fi
 
+logv "Generating CSR Private key: ${domain}.key"
 openssl genrsa -out ${domain}.key 4096
 
 # Generate a configuration that includes the SAN subject alt name required by Chrome and well, most browsers
 # these days
 opensslconfig=$(mktemp)
+
+if [ "${altnames}X" = "X" ]; then
+    altnames=${domain}
+fi
+
+i=1
+dnsentries=""
+for dns in ${altnames}; do
+    dnsentries="${dnsentries}DNS.${i} = ${dns}
+"
+    i=$((i + 1))
+done
+
 
 cat << EOF > ${opensslconfig}
 [req]
@@ -48,7 +149,7 @@ C=${C}
 ST=${ST}
 L=
 O=${O}
-OU=
+OU=${OU}
 emailAddress=brian@int.bjs
 CN=${CN}
 
@@ -56,11 +157,12 @@ CN=${CN}
 subjectAltName = @alt_names
 
 [ alt_names ]
-DNS.1 = ${domain}
+${dnsentries}
 EOF
 
-echo  "Generating CSR"
+logv "Using openssl configuration" "${opensslconfig}"
 
+logv "Generating csr ${domain}.csr"
 openssl req -new -nodes -sha256 \
     -key ${domain}.key \
     -subj "/C=${C}/ST=${ST}/O=${O}/CN=${CN}" \
@@ -73,10 +175,12 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "INFO: CSR Info:"
-openssl req -in ${domain}.csr -noout -text
+if ${verbose}; then
+    logv "CSR Information"
+    openssl req -in ${domain}.csr -noout -text
+fi
 
-echo "INFO: Generating Certificate"
+logv "Generating service certificate: ${domain}.crt"
 openssl x509 -req -in ${domain}.csr \
     -CA rootca.crt -CAkey rootca.key -CAcreateserial \
     -extfile ${opensslconfig} \
